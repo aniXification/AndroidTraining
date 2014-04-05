7a7e4e43898efe169d03987d055d53b8c007665d
Anish Shrestha <er.anishshrestha@gmail.com>
2014-04-05 08:04:15 +0545

    z

--- AndroidTraining1/gen/com/at/androidtraining1/R.java
+++ AndroidTraining1/gen/com/at/androidtraining1/R.java
@@ -24,7 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080006;
+        public static final int btn_database=0x7f080002;
+        public static final int btn_listview=0x7f080000;
+        public static final int btn_shared_prefs=0x7f080003;
+        public static final int btn_styling=0x7f080001;
+        public static final int btn_web_services=0x7f080005;
+        public static final int btn_webview=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
